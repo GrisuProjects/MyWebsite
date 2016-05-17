4f53ace42dd2d2c23c81e7d88b8a0586e17eb5b2
Christian Kaindl <crisscross.kaindl@outlook.de>
2016-05-17 11:24:48 +0200

    Add Worker for time

--- scripts/main.js
+++ scripts/main.js
@@ -16,39 +16,28 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this website.  If not, see <http://www.gnu.org/licenses/>.
 */
-'use strict'; // TODO: function form of 'use strict'
+'use strict';
 
 var colors = ["forestgreen", 'rgba(237, 177, 26, 1)', 'rgba(65, 119, 153, 1)'];
 
 function currentTime() { // TODO: Worker
+    var w;
 
-  // Display current time
-    var  hour = new Date().getHours(),
-        min = new Date().getMinutes(),
-        sec = new Date().getSeconds(),
-        time = document.getElementsByTagName('time')[0];
-
-    // Update current Time
-    setInterval(function () {
-        sec++;
-
-        if (sec == 60) {
-            sec = 0;
-            min++;
-            if (min == 60) {
-                min = 0;
-                hour++;
-                if (hour == 24) {
-                    hour = 0;
-                }
-            }
+    if (typeof(Worker) !== 'undefined') {
+        if (typeof(w) == 'undefined') { // Look if this worker is in use
+            w = new Worker('scripts/worker.js');
         }
-        time.innerHTML = ('0' + hour).slice(-2) + ':' + ('0' + min).slice(-2) + ':' + ('0' + sec).slice(-2);
-    }, 1000);
+        w.onmessage = function(event) {
+            document.getElementsByTagName('TIME')[0].innerHTML = event.data;
+        };
+    } else {
+        console.log("No Worker support: Cannot dislay time");
+    }
 
-    setTimeout(function () {
-      document.getElementsByTagName('time')[0].style.height = '2em';
-    }, 1000);
+    // Reveal time
+    setTimeout(function() {
+        document.getElementsByTagName('time')[0].style.height = '2em';
+    }, 1500);
 }
 // ===============================================================================
 function footerSize() {
@@ -85,16 +74,16 @@ function greatView(clicked) {
         active = clicked;
     }
     // set the proper height
-        var measure = document.getElementsByClassName("measuringWrapper")[clicked];
-        grow[clicked].style.height = measure.clientHeight + 'px';
+    var measure = document.getElementsByClassName("measuringWrapper")[clicked];
+    grow[clicked].style.height = measure.clientHeight + 'px';
 
     /*http://stackoverflow.com/a/13938747*/
 }
 
 function greatViewResize() {
-    setTimeout(function () {
-      var measure = document.getElementsByClassName("measuringWrapper")[active];
-      document.getElementsByClassName('sectionText')[active].style.height = measure.clientHeight + 'px';
+    setTimeout(function() {
+        var measure = document.getElementsByClassName("measuringWrapper")[active];
+        document.getElementsByClassName('sectionText')[active].style.height = measure.clientHeight + 'px';
     }, 400);
 }
 
